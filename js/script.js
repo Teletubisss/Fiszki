1e1a7f10bca4c03cc3ec47487bb3945df5758433
Teletubisss <natalagorny@gmail.com>
2025-02-17 21:22:25 +0100

    finally got the js part done - left the popup

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 let cardRow = document.querySelector('#cardRow');
 
+window.onload = function() {
+
+}
+
 let keyDownTitle = (e) => {
     if (e.keyCode === 13) {
         e.preventDefault();
@@ -9,74 +13,67 @@ let keyDownTitle = (e) => {
     }
 }
 
-let smallCard = (value, descr) => {
+let smallCard = (valueFront, descrFront, valueBack, descrBack) => {
     return `<div class="col-lg-3 col-md-4 col-6">
-                    <div class="card p-1 mb-3">
-                            <div class="card-body d-flex flex-column justify-content-center align-items-center p-3">
-                                <button type="button" onclick="Popup()" class="btn btn-secondary d-flex justify-content-between m-2">EDIT</button>
-                              <h5 class="card-title m-3">${value}</h5>
-                              <p class="card-text">${descr}</p>
+                    <div class="card  p-4 mb-3">
+                        <div>
+                            <div class="card-body d-flex flex-column justify-content-center align-items-center p-5">
+                                    <button onclick="saveCard('#titleInput', '#descriptionInput')" type="button" class="btn btn-secondary d-flex justify-content-between m-2">EDIT</button>
+                              <h5>${valueFront}</h5>
+                              <p>${descrFront}</p>
                             </div>
+                        </div>
+                        <div class="hidden">
+                            <div class="card-body d-flex flex-column justify-content-center align-items-center p-5">
+                              <h5>${valueBack}</h5>
+                              <p>${descrBack}</p>
+                            </div>
+                        </div>
                     </div>
                 </div>`;
 }
 
-let addCard = (value, descr) => {
-    if (value !== '') {
-        cardRow.innerHTML += smallCard(value, descr);
+
+let addCard = (valueFront, descrFront, valueBack, descrBack) => {
+    if (valueFront !== '' && valueBack !== '') {
+        cardRow.innerHTML += smallCard(valueFront, descrFront, valueBack, descrBack);
     }
 }
 
-let keyDownCardTitle = (e) => {
+let titleKeyDown = (e, descriptionSelectorId) => {
     if (e.keyCode === 13) {
-        e.preventDefault();
-
-        let bigCardDesc = document.querySelector("#descriptionInput");
+        let bigCardDesc = document.querySelector(descriptionSelectorId);
         bigCardDesc.focus();
     }
 }
 
-let keyDown = (e) => {
+let descriptionKeyDown = (e, titleSelectorId, decriptionSelectorId) => {
     if (e.keyCode === 13) {
-        e.preventDefault();
-        let bigCardText = document.querySelector("#titleInput");
-        let bigCardDesc = document.querySelector("#descriptionInput");
-
-        addCard(bigCardText.value, bigCardDesc.value);
-        bigCardDesc.value = '';
-        bigCardText.value = '';
-        bigCardText.focus();
+        saveCard(titleSelectorId, decriptionSelectorId)
     }
 }
-let SaveButton = () => {
-        let bigCardText = document.querySelector("#titleInput");
-        let bigCardDesc = document.querySelector("#descriptionInput");
 
-        addCard(bigCardText.value, bigCardDesc.value);
-        bigCardDesc.value = '';
-        bigCardText.value = '';
-        bigCardText.focus();
+let saveCard = (titleSelectorIdFront, decriptionSelectorIdFront, titleSelectorIdBack, descriptionSelectorIdBack) => {
+    let bigCardTextFront = document.querySelector(titleSelectorIdFront);
+    let bigCardDescFront = document.querySelector(decriptionSelectorIdFront);
+    let bigCardTextBack = document.querySelector(titleSelectorIdBack);
+    let bigCardDescBack = document.querySelector(descriptionSelectorIdBack);
+
+    addCard(bigCardTextFront.value, bigCardDescFront.value, bigCardTextBack.value, bigCardDescBack.value);
+    bigCardDescFront.value = '';
+    bigCardTextFront.value = '';
+    bigCardDescBack.value = '';
+    bigCardTextBack.value = '';
+    bigCardText.focus();
 }
 
-window.onload = function() {
-
+let flipCard = (activeClass, noneClass) => {
+    let active = document.querySelector(activeClass);
+    let none = document.querySelector(noneClass);
+    active.classList.add('hidden');
+    none.classList.remove('hidden');
 }
-
-let flipCardBack = () => {
-    let back = document.getElementById("back");
-    let front = document.getElementById("front");
-    back.classList.remove("hidden");
-    front.classList.add('hidden');
-}
-let flipCardFront = () => {
-    let back = document.getElementById("back");
-    let front = document.getElementById("front");
-    front.classList.remove("hidden");
-    back.classList.add('hidden');
-}
-
-
 
 let Popup = () => {
-    
+
 } 
