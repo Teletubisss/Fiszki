26566d6aae7f4219827d7278cb3ac0b444fa154c
mgorny <michal.gorny@ec3networks.at>
2025-03-14 23:03:34 +0100

    fixed lesson

--- js/scriptYourLesson.js
+++ js/scriptYourLesson.js
@@ -1,66 +1,43 @@
 window.onload = function() {
-    createEmptyArrays();
     showRandomCard();
 }
 
 let currentProjectName = JSON.parse(localStorage.getItem('currentProjectName'));
 let projectData = JSON.parse(localStorage.getItem(currentProjectName));
-let correctAnswers = JSON.parse(localStorage.getItem("correctAnswers") || "[]");
-let wrongAnswers = JSON.parse(localStorage.getItem("wrongAnswers") || "[]");
+let correctAnswers = [];
+let wrongAnswers = [];
 let currentCard;
 
-
-let createEmptyArrays = () => {
-        localStorage.setItem("correctAnswers", JSON.stringify([]));
-        localStorage.setItem("wrongAnswers", JSON.stringify([]));   
-};
-
-
 let moveToCategory = (category) => {
-
-    if (projectData.flips.length === 0 && wrongAnswers.length > 0) {
-        debugger;
-        let projectTitle = localStorage.currentProjectName.value;
-            projectData.flips = [...wrongAnswers];
-            console.log(projectData)
-            localStorage.setItem(currentProjectName, JSON.stringify(projectData));
-            localStorage.setItem('currentProjectName', JSON.stringify(projectTitle));
-            localStorage.setItem("wrongAnswers", JSON.stringify([]));
-        }
-    else {
-        if (category === "correctAnswers") {
-            correctAnswers.push(currentCard);
-        } 
-        if (category === "wrongAnswers") {
-            wrongAnswers.push(currentCard);
-        }
-        if (projectData.flips.includes(currentCard)) {
-            projectData.flips = projectData.flips.filter(card => card !== currentCard);
-        } 
-        else if (projectData.wrongAnswers.includes(currentCard)) {
-            projectData.wrongAnswers = projectData.wrongAnswers.filter(card => card !== currentCard);
+    if (category === "correctAnswers")
+        correctAnswers.push(currentCard);
     
+    if (category === "wrongAnswers")
+        wrongAnswers.push(currentCard);
     
-        }  
-
-
-        localStorage.setItem("correctAnswers", JSON.stringify(correctAnswers));
-        localStorage.setItem("wrongAnswers", JSON.stringify(wrongAnswers));
-        localStorage.setItem(currentProjectName, JSON.stringify(projectData));
+    if (projectData.flips.includes(currentCard))
+        projectData.flips = projectData.flips.filter(card => card !== currentCard); //removes current from flips
+    
+    if (projectData.flips.length === 0 && wrongAnswers.length > 0) { //if no more flips
+        projectData.flips = [...wrongAnswers]; //add wrongs  to flips
+        wrongAnswers= []; //and clear wrongs
     }
 
+    console.log('data: ' + projectData.flips.length + ', correct: ' + correctAnswers.length + 'wrong: ' + wrongAnswers.length); //log just to know how many where are left (for easier testing)
 
     showRandomCard();
-
 }
 
-
 let showRandomCard = () => {
-    let cards = projectData.flips.length > 0 ? projectData.flips : wrongAnswers;
+    let cards = projectData.flips.length > 0 ? projectData.flips : [];
     currentCard = cards[Math.floor(Math.random() * cards.length)];
-    console.log(currentCard)
-    document.querySelector("#startLessonTitleFront").innerText = currentCard.titleFront;
-    document.querySelector("#startLessonDescFront").innerText = currentCard.descriptionFront;
-    document.querySelector("#startLessonTitleBack").innerText = currentCard.titleBack;
-    document.querySelector("#startLessonDescBack").innerText = currentCard.descriptionBack;
+
+    if (currentCard !== undefined) {
+        document.querySelector("#startLessonTitleFront").innerText = currentCard.titleFront;
+        document.querySelector("#startLessonDescFront").innerText = currentCard.descriptionFront;
+        document.querySelector("#startLessonTitleBack").innerText = currentCard.titleBack;
+        document.querySelector("#startLessonDescBack").innerText = currentCard.descriptionBack;
+    } else {
+        swal.fire('Endo :)'); //no more to show - ENDO :)
+    }
 }
